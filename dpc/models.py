5fd33fcde7c26751f11459d1df24f3cdcb79e79e
Jeffrey Johnson <ortelius@gmail.com>
2015-06-14 11:43:57 -0400

    More model fixup

--- dpc/models.py
+++ dpc/models.py
@@ -1,20 +1,20 @@
 from django.db import models
  
-class Unite_Mesure(models.model):
+class Unite_Mesure(models.Model):
     description = models.CharField(max_length=50)
     isBase = models.BooleanField()
-    ref = models.ForeignKey(Unite_Mesure)
-    valeur_ref = models.DecimalField()
+    #ref = models.ForeignKey(Unite_Mesure)
+    valeur_ref = models.FloatField()
 	 
-class Menaces(models.model):
+class Menaces(models.Model):
     nom = models.CharField(max_length=50)
     caracteristiques= models.ForeignKey("Caract_Menaces")
     duree = models.DurationField()
 	 
-class Caract_Menaces(models.model):
+class Caract_Menaces(models.Model):
     description = models.CharField(max_length=50)
     estEsprimee = models.ForeignKey(Unite_Mesure)
 	 
-class Risques(models.model):
+class Risques(models.Model):
     description = models.CharField(max_length=50)
     menaces= models.ForeignKey("Menaces")
