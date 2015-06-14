7ba4557bbb32984a54e452d3f21c077aacb10366
Anne Martine <augustinannemartine@gmail.com>
2015-06-14 11:33:36 -0400

    Adding Models

--- dpc/models.py
+++ dpc/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+ 
+class Unite_Mesure(models.model):
+     description = models.CharField(max_length=50)	 
+	 isBase = models.BooleanField()
+	 ref = models.ForeignKey(Unite_Mesure)
+	 valeur_ref = models.DecimalField()
+	 
+class Menaces(models.model):
+     nom = models.CharField(max_length=50)
+     caracteristiques= models.ForeignKey("Caract_Menaces")
+	 duree = models.DurationField()
+	 
+class Caract_Menaces(models.model):
+     description = models.CharField(max_length=50)	 
+	 estEsprimee = models.ForeignKey(Unite_Mesure)
+	 
+class Risques(models.model):
+     description = models.CharField(max_length=50)
+     menaces= models.ForeignKey("Menaces")
+	 
+	  
