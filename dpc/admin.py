97904388665138c98e54e7ac1668a187a6fb6565
Anne Martine <augustinannemartine@gmail.com>
2015-06-14 13:04:55 -0400

    Fix merge issue

--- dpc/admin.py
+++ dpc/admin.py
@@ -1,14 +1,10 @@
 from django.contrib.gis import admin
 
-<<<<<<< HEAD
-from .models import Unite_Mesure, Menaces, Caract_Menaces, Risques
-=======
-from models import Unite_Mesure, Menaces, Caract_Menaces, Risques, Commune
+from .models import Unite_Mesure, Menaces, Caract_Menaces, Risques, Commune
 
 class CommuneAdmin(admin.OSMGeoAdmin):
     list_display = ('commune','departemen',)
     search_fields = ('commune', 'departemen',)
->>>>>>> origin/master
 
 admin.site.register(Unite_Mesure)
 admin.site.register(Menaces)
