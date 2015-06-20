c24388de9d8176a1ca64ec6c4a60401506cd3a01
winchelo <abelowi@yahoo.fr>
2015-06-20 14:17:54 -0400

    Revert "local setting"
    This reverts commit 31e1648bcdce9d2b0cf31695a9d7f65b3e518c1b.
    
    Conflicts:
    	sovelavi/local_settings.py

--- sovelavi/settings.py
+++ sovelavi/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sovelavi',
 		'USER': 'Martine',
-        'PASSWORD': 'test',
+        'PASSWORD': 'martine29101991',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
