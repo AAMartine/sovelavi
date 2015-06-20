4590b4eafef6b625c5acd4dc0be4325e35727759
Anne Martine <augustinannemartine@gmail.com>
2015-06-20 13:32:42 -0400

    Change on local_settings

---
+++ sovelavi/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sovelavi',
+		'USER': 'Martine',
+        'PASSWORD': 'martine29101991',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
+    }
+}
